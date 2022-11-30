690be40b961ed8eaf91ce70911bce511178da31b
dtp06077 <dtp06077@naver.com>
2022-11-30 20:42:57 +0900

    FIX VOLLEY ERROR

---
+++ app/src/main/java/com/example/saferecorder/ValidateRequest.java
@@ -0,0 +1,16 @@
+package com.example.saferecorder;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ValidateRequest {
+    //서버 url 설정(php파일 연동)
+    final static  private String URL="http://cpcp1245.dothome.co.kr/UserValidate.php";
+    private Map<String,String> map;
+
+}
