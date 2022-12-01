021ea6d03fd6f5af613c91172bf68f4ac6c1b196
dtp06077 <dtp06077@naver.com>
2022-12-01 16:51:38 +0900

    REFACTOR REGISTERACTIVITY

--- app/src/main/java/com/example/saferecorder/RegisterActivity.java
+++ app/src/main/java/com/example/saferecorder/RegisterActivity.java
@@ -1,10 +1,28 @@
 package com.example.saferecorder;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText et_id, et_pass, et_name, et_age,et_hak,et_major,et_passck;
+    private Button btn_register,validateButton;
+    private AlertDialog dialog;
+    private boolean validate=false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
