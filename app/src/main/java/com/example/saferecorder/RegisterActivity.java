98ff19e21deeb6b4e268aa80f52634e3fbe9b060
dtp06077 <dtp06077@naver.com>
2022-12-02 01:21:51 +0900

    AVAILABLE ID CHECK

--- app/src/main/java/com/example/saferecorder/RegisterActivity.java
+++ app/src/main/java/com/example/saferecorder/RegisterActivity.java
@@ -53,5 +53,21 @@ public class RegisterActivity extends AppCompatActivity {
                     dialog.show();
                     return;
                 }
+                Response.Listener<String> responseListener=new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse=new JSONObject(response);
+                            boolean success=jsonResponse.getBoolean("success");
+                            if(success){
+                                AlertDialog.Builder builder=new AlertDialog.Builder( RegisterActivity.this );
+                                dialog=builder.setMessage("사용할 수 있는 아이디입니다.")
+                                        .setPositiveButton("확인",null)
+                                        .create();
+                                dialog.show();
+                                et_id.setEnabled(false);
+                                validate=true;
+                                validateButton.setText("확인");
+                            }
     }
 }
