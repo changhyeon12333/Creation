75be35653b91daa553bff12859931190d0d1294d
dtp06077 <dtp06077@naver.com>
2022-12-01 16:15:59 +0900

    FEAT REGISTERREQUEST

---
+++ app/src/main/java/com/example/saferecorder/RegisterRequest.java
@@ -0,0 +1,11 @@
+package com.example.saferecorder;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegisterRequest extends StringRequest {
+}
--- app/src/main/java/com/example/saferecorder/ValidateRequest.java
+++ app/src/main/java/com/example/saferecorder/ValidateRequest.java
@@ -8,7 +8,7 @@ import com.android.volley.toolbox.StringRequest;
 import java.util.HashMap;
 import java.util.Map;
 
-public class ValidateRequest {
+public class ValidateRequest extends StringRequest {
     //서버 url 설정(php파일 연동)
     final static  private String URL="http://cpcp1245.dothome.co.kr/UserValidate.php";
     private Map<String,String> map;
