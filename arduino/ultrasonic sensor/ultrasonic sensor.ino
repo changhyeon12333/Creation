63f92d258370b7be2dbca75c8c355c62012fc374
Lee chang <changhyeon12333@gmail.com>
2022-12-04 18:27:45 +0900

    double sensing

--- arduino/ultrasonic sensor/ultrasonic sensor.ino
+++ arduino/ultrasonic sensor/ultrasonic sensor.ino
@@ -3,15 +3,17 @@
 #include<SoftwareSerial.h>
 SoftwareSerial HM10 (6,7);
 
-const int trig =11;
-const int echo =12;
+const int trig_0 = 9;
+const int echo_0 = 8;
+const int trig_1 =11;
+const int echo_1=12;
 
 
 void setup() {
-
-
-  pinMode(trig,OUTPUT);
-  pinMode(trig,INPUT);
+  pinMode(trig_0,OUTPUT);
+  pinMode(trig_0,INPUT);
+  pinMode(trig_1,OUTPUT);
+  pinMode(trig_1,INPUT);
   Serial.begin(9600);
   HM10.begin(9600);
 
@@ -25,17 +27,25 @@ void loop() {
     HM10.write(Serial.read());
 
   //신호발생
-  digitalWrite(trig, LOW);
-  delayMicroseconds(1000);
-  digitalWrite(trig,HIGH);
-  delayMicroseconds(1000);
-  digitalWrite(trig,LOW);
-  //신호입력
-  long Length = pulseIn(echo, HIGH);
-  long distance = (Length/2) /29.1;
-  Serial.print(distance);
-  Serial.println(" Cm");
+  digitalWrite(trig_0, LOW);
+  digitalWrite(trig_1, LOW);
+  delayMicroseconds(100);
+  digitalWrite(trig_0,HIGH);
+  digitalWrite(trig_1,HIGH);
+  delayMicroseconds(100);
+  digitalWrite(trig_0,LOW);
+  digitalWrite(trig_1,LOW);
 
+  //신호입력
+  long Length_0 = pulseIn(echo_0, HIGH);
+  long distance_0 = (Length_0/2) /29.1;
+  long Length_1 = pulseIn(echo_1, HIGH);
+  long distance_1 = (Length_1/2) /29.1;
+  
+  Serial.print("sensor_0 :");
+  Serial.println(distance_0);
+  Serial.print("sensor_1 :");
+  Serial.println(distance_1);
 
 
 }
