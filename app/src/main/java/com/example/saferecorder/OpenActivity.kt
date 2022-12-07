961117ad9009dfd460bddf1aa2788222e8336c1b
dtp06077 <dtp06077@naver.com>
2022-12-07 17:03:09 +0900

    FEAT ACTIVITY EVENT

--- app/src/main/java/com/example/saferecorder/OpenActivity.kt
+++ app/src/main/java/com/example/saferecorder/OpenActivity.kt
@@ -1,11 +1,30 @@
 package com.example.saferecorder
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 
-class OpenActivity : AppCompatActivity() {
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.inappmessaging.model.Button
+
+
+public class OpenActivity : AppCompatActivity() {
+
+    private var btn_login: Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_open)
+        btn_login = findViewById<Button>(R.id.lg_saferecorder_btn)
+
+        val private = null
+        private Button btn_login;
+        // 운전시작 버튼 클릭시 서비스 페이지로 이동
+        btn_login.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this@OpenActivity, LoginActivity::class.java)
+            startActivity(intent)
+        })
+
     }
 }
+
