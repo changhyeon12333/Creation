6783d37a42da00c1f0f328c41d29936fdcae477f
dtp06077 <dtp06077@naver.com>
2022-12-09 05:24:35 +0900

    FEAT KAKAO LOGIN

--- app/src/main/java/com/example/saferecorder/OpenActivity.java
+++ app/src/main/java/com/example/saferecorder/OpenActivity.java
@@ -20,28 +20,12 @@ import java.security.MessageDigest;
 
 
 public class OpenActivity extends AppCompatActivity {
-    private void getAppKeyHash() {
-        try {
-            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
-            for (Signature signature : info.signatures) {
-                MessageDigest md;
-                md = MessageDigest.getInstance("SHA");
-                md.update(signature.toByteArray());
-                String something = new String(Base64.encode(md.digest(), 0));
-                Log.e("Hash key", something);
-            }
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            Log.e("name not found", e.toString());
-        }
-    }
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_open);
-        getAppKeyHash();
         @SuppressLint("WrongViewCast") Button button = findViewById(R.id.lg_saferecorder_btn);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
