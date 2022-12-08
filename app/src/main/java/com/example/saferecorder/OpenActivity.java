00a39bd716b446fd69e5f62de8f03caebe210356
dtp06077 <dtp06077@naver.com>
2022-12-09 05:04:12 +0900

    FEAT KAKAO LOGIN

--- app/src/main/java/com/example/saferecorder/OpenActivity.java
+++ app/src/main/java/com/example/saferecorder/OpenActivity.java
@@ -1,20 +1,47 @@
 package com.example.saferecorder;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.security.MessageDigest;
+
+
 public class OpenActivity extends AppCompatActivity {
+    private void getAppKeyHash() {
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md;
+                md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                String something = new String(Base64.encode(md.digest(), 0));
+                Log.e("Hash key", something);
+            }
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            Log.e("name not found", e.toString());
+        }
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_open);
-
+        getAppKeyHash();
         @SuppressLint("WrongViewCast") Button button = findViewById(R.id.lg_saferecorder_btn);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
