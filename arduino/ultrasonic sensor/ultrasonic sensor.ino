1dfb30a9f7d9307daa276adca06c9d53738314ad
Lee chang <changhyeon12333@gmail.com>
2022-12-09 00:31:34 +0900

    blue

--- arduino/ultrasonic sensor/ultrasonic sensor.ino
+++ arduino/ultrasonic sensor/ultrasonic sensor.ino
@@ -1,9 +1,9 @@
 #include <SoftwareSerial.h>
+const int tx =2;
+const int rx =3;
+SoftwareSerial HM10 (tx,rx);
 
-#include<SoftwareSerial.h>
-SoftwareSerial HM10 (6,7);
-
-const int trig_0 = 9;
+const int trig_0 = 7;
 const int echo_0 = 8;
 const int trig_1 =11;
 const int echo_1=12;
@@ -16,15 +16,9 @@ void setup() {
   pinMode(trig_1,INPUT);
   Serial.begin(9600);
   HM10.begin(9600);
-
 }
 
 void loop() {
-  //Bluth
-  if(HM10.available())
-    Serial.write(HM10.read());
-  if(Serial.available())
-    HM10.write(Serial.read());
 
   //신호발생
   digitalWrite(trig_0, LOW);
@@ -36,7 +30,7 @@ void loop() {
   digitalWrite(trig_0,LOW);
   digitalWrite(trig_1,LOW);
 
-  //신호입력
+//   //신호입력
   long Length_0 = pulseIn(echo_0, HIGH);
   long distance_0 = (Length_0/2) /29.1;
   long Length_1 = pulseIn(echo_1, HIGH);
@@ -46,6 +40,13 @@ void loop() {
   Serial.println(distance_0);
   Serial.print("sensor_1 :");
   Serial.println(distance_1);
+    //Bluth
+  if (HM10.available()){ 
+    byte data = HM10.read();
+    Serial.write(data);
+  }
 
+  HM10.write(distance_0); 
+  HM10.write(distance_1);
 
 }
