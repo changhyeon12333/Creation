4da9454ffd5ece6256b526023fd9cdebd9f40d12
Lee chang <changhyeon12333@gmail.com>
2022-12-09 05:25:21 +0900

    FEAT BLUETOOTH GETVIEW

--- app/src/main/java/com/example/ble_train/DeviceListAdapter.java
+++ app/src/main/java/com/example/ble_train/DeviceListAdapter.java
@@ -14,4 +14,22 @@ public class DeviceListAdapter extends ArrayAdapter<BluetoothDevice> {
         mLayoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         mViewResourceId = resourceId;
     }
+    public View getView(int position , View convertView, ViewGroup parent){
+        convertView = mLayoutInflater.inflate(mViewResourceId,null);
+        BluetoothDevice device = mDevices.get(position);
+
+        if(device!=null){
+            TextView deviceName = (TextView) convertView.findViewById(R.id.tvDeviceName);
+            TextView deviceAddress = (TextView) convertView.findViewById(R.id.tvDeviceAddress);
+            //원래 null검사 해야하는데 검색되는 것중 null인게 있어서 뺌
+            //if(deviceName !=null){
+            deviceName.setText(device.getName());
+            //}
+            //else if(deviceAddress!=null){
+            deviceAddress.setText(device.getAddress());
+            //}
+        }
+        return convertView;
+    }
+
 }
