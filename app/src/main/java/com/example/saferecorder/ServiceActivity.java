cb78f13bfb7cf18c845c8170d840f2887bb2bf91
Lee chang <changhyeon12333@gmail.com>
2022-12-09 03:22:57 +0900

    FEAT BLUETOOTH ACTI

--- app/src/main/java/com/example/saferecorder/ServiceActivity.java
+++ app/src/main/java/com/example/saferecorder/ServiceActivity.java
@@ -1,6 +1,15 @@
 package com.example.saferecorder;
+import android.bluetooth.BluetoothAdapter;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class ServiceActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
+public class ServiceActivity extends AppCompatActivity {
+    Button btnBluetoothOn;
+    Button btnBluetoothOff;
+
+    TextView tvBluetoothStatus;
+    BluetoothAdapter mBluetoothAdapter;
 
 }
