18bb59c5dfa1615ab283ace3f3124028630dcae7
Lee chang <changhyeon12333@gmail.com>
2022-12-09 05:26:17 +0900

    FEAT BLUETOOTH IMPTCS

--- app/src/main/java/com/example/ble_train/DeviceListAdapter.java
+++ app/src/main/java/com/example/ble_train/DeviceListAdapter.java
@@ -1,7 +1,14 @@
 package com.example.ble_train;
 
 import android.bluetooth.BluetoothDevice;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class DeviceListAdapter extends ArrayAdapter<BluetoothDevice> {
     private LayoutInflater mLayoutInflater;
