a8393ab6b3a0e6ff7a565446aa5cfd0eaa29b83f
Lee chang <changhyeon12333@gmail.com>
2022-12-09 03:25:15 +0900

    FEAT BLUETOOTH ONCE

--- app/src/main/java/com/example/saferecorder/ServiceActivity.java
+++ app/src/main/java/com/example/saferecorder/ServiceActivity.java
@@ -1,5 +1,6 @@
 package com.example.saferecorder;
 import android.bluetooth.BluetoothAdapter;
+import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -12,4 +13,12 @@ public class ServiceActivity extends AppCompatActivity {
     TextView tvBluetoothStatus;
     BluetoothAdapter mBluetoothAdapter;
 
-}
+    final static int BT_REQUEST_ENABLE = 1;
+    final static int BT_MESSAGE_READ = 2;
+    final static int BT_CONNECTING_STATUS = 3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+        }
+    }
