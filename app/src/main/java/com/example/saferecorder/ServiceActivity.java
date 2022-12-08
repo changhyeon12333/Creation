53c9c116492b7f2832ca621287729520a555a092
Lee chang <changhyeon12333@gmail.com>
2022-12-09 03:13:17 +0900

    FEAT BLUETOOTH ANDROID

--- app/src/main/java/com/example/saferecorder/ServiceActivity.java
+++ app/src/main/java/com/example/saferecorder/ServiceActivity.java
@@ -1,15 +1,6 @@
 package com.example.saferecorder;
 
-public class ServiceActivity extends AppCompatActivity{
-    Button btnBluetoothOn;
-    Button btnBluetoothOff;
+public class ServiceActivity {
 
-    TextView tvBluetoothStatus;
-    BluetoothAdapter mBluetoothAdapter;
-
-    //Mode
-    final static int BT_REQUEST_ENABLE = 1;
-    final static int BT_MESSAGE_READ = 2;
-    final static int BT_CONNECTING_STATUS = 3;
 
 }
