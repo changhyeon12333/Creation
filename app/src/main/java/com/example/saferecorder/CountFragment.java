40a0126b530ad4b3f816786348fbe3b5964f9fa0
dtp06077 <dtp06077@naver.com>
2022-12-09 16:58:11 +0900

    REFACTORY SETTING FRAGMENT

---
+++ app/src/main/java/com/example/saferecorder/CountFragment.java
@@ -0,0 +1,6 @@
+package com.example.saferecorder;
+
+import androidx.fragment.app.Fragment;
+
+public class CountFragment extends Fragment {
+}
--- app/src/main/java/com/example/saferecorder/SettingActivity.java
+++ app/src/main/java/com/example/saferecorder/SettingActivity.java
@@ -16,6 +16,7 @@ public class SettingActivity extends AppCompatActivity {
 
     private Button btn_close;
     private Button btn_priv;
+    private Button btn_count;
 
     @SuppressLint("WrongViewCast")
     @Override
@@ -25,6 +26,7 @@ public class SettingActivity extends AppCompatActivity {
 
         btn_close=findViewById(R.id.app_down_btn);
         btn_priv=findViewById(R.id.priv_cng_btn);
+        btn_count=findViewById(R.id.count_check_btn);
 
         btn_close.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,20 +41,27 @@ public class SettingActivity extends AppCompatActivity {
                 viewMypage(0);
             }
         });
+        // BL,AL값 확인 클릭시 프레그먼트 변환
+        btn_count.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                viewMypage(1);
+            }
+        });
     }
-}
     private void viewMypage(int whr) {
         if(whr == 0) {      // 개인정보변경
             FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-            PrivacyFragment myFragment = new PrivacyFragment();
+            PrivacyFragment PrFragment = new PrivacyFragment();
             transaction.replace(R.id.frame, PrFragment);
             transaction.addToBackStack(null);
             transaction.commit();
         } else if(whr == 1) {     //AL,BL값 확인
             FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-            CountFragment optFragment = new CountFragment();
+            CountFragment CtFragment = new CountFragment();
             transaction.replace(R.id.frame, CtFragment);
             transaction.addToBackStack(null);
             transaction.commit();
         }
     }
+}
