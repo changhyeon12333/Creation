f1b2cbf26bb4dfb569dfad0ff57f8f19aa7e6988
dtp06077 <dtp06077@naver.com>
2022-12-09 16:30:26 +0900

    REFACTORY SETTING ACTIVITY

--- app/src/main/java/com/example/saferecorder/SettingActivity.java
+++ app/src/main/java/com/example/saferecorder/SettingActivity.java
@@ -1,7 +1,9 @@
 package com.example.saferecorder;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentTransaction;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
@@ -13,6 +15,7 @@ import android.widget.Toast;
 public class SettingActivity extends AppCompatActivity {
 
     private Button btn_close;
+    private Button btn_priv;
 
     @SuppressLint("WrongViewCast")
     @Override
@@ -21,11 +24,19 @@ public class SettingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_setting);
 
         btn_close=findViewById(R.id.app_down_btn);
+        btn_priv=findViewById(R.id.priv_cng_btn);
 
         btn_close.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+        // 마이페이지 클릭시 프레그먼트 변환
+        btn_priv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                viewMypage(0);
             }
         });
     }
