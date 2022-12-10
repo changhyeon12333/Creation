ad422a788b13ba44c236abb041dcf4476c421c11
changhyeon12333 <ckd5508@naver.com>
2022-12-10 17:25:44 +0900

    gps tracker

--- app/src/main/java/com/example/saferecorder/GpsTracker.java
+++ app/src/main/java/com/example/saferecorder/GpsTracker.java
@@ -4,3 +4,8 @@ import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.IBinder;
+import android.util.Log;
