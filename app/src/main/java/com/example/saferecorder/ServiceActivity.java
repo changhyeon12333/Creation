4f1e696d1d536f325c8cdc199d510250bbc1425b
changhyeon12333 <ckd5508@naver.com>
2022-12-10 16:54:02 +0900

    블루투스 구현

---
+++ app/src/main/java/com/example/saferecorder/ServiceActivity.java
@@ -0,0 +1,8 @@
+package com.example.saferecorder;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
