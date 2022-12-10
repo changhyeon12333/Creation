57b73e5b72e82f7127fcfe43d178eb62276f8e99
changhyeon12333 <ckd5508@naver.com>
2022-12-10 17:28:42 +0900

    gps tracker

--- app/src/main/java/com/example/saferecorder/GpsTracker.java
+++ app/src/main/java/com/example/saferecorder/GpsTracker.java
@@ -63,3 +63,16 @@ public class GpsTracker extends Service implements LocationListener {
                         location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
                         if (location != null)
                         {
+                            latitude = location.getLatitude();
+                            longitude = location.getLongitude();
+                        }
+                    }
+                }
+
+
+                if (isGPSEnabled)
+                {
+                    if (location == null)
+                    {
+                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME_BW_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                        if (locationManager != null)
