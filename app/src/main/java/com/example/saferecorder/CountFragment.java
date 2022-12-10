f2b5b4e3249c10ada8496e7481be1ffac31bb19b
dtp06077 <dtp06077@naver.com>
2022-12-10 21:45:50 +0900

    FEAT COUNTFRAGMENT

--- app/src/main/java/com/example/saferecorder/CountFragment.java
+++ app/src/main/java/com/example/saferecorder/CountFragment.java
@@ -1,6 +1,27 @@
 package com.example.saferecorder;
 
 import androidx.fragment.app.Fragment;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
+
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+
+import com.example.saferecorder.DBHelper;
 
 public class CountFragment extends Fragment {
 
