3e2475be10c3ff7168ada11b0eda6af8f187c9a0
changhyeon12333 <ckd5508@naver.com>
2022-12-10 17:08:24 +0900

    블루투스 구현

--- app/src/main/java/com/example/saferecorder/ServiceActivity.java
+++ app/src/main/java/com/example/saferecorder/ServiceActivity.java
@@ -366,3 +366,11 @@ public class ServiceActivity extends AppCompatActivity {
             public void onClick(DialogInterface dialog, int id) {
                 dialog.cancel();
             }
+        });
+        builder.create().show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
